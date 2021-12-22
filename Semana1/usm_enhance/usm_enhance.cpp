7024b226b488bd8ce9ad85c21ae2940b8ef96b58
JoseManuelAlcaldeLlergo <josemanuelalcaldellergo@gmail.com>
2021-12-22 13:25:10 +0100

    usm_enhace interactivo solo radio

--- Semana1/usm_enhance/usm_enhance.cpp
+++ Semana1/usm_enhance/usm_enhance.cpp
@@ -29,16 +29,82 @@ const cv::String keys =
     "{c circular     |      | Use circular convolution.}"
     "{f filter       |0     | Filter type: 0->Box, 1->Gaussian. Default 0.}"
     "{@input         |<none>| input image.}"
-    "{@output        |<none>| output image.}"
-    ;
+    "{@output        |<none>| output image.}";
 
-
-int
-main (int argc, char* const* argv)
+/**
+ * @brief Application State.
+ * Use this structure to maintain the state of the application
+ * that will be passed to the callbacks.
+ */
+struct UserData
 {
-    int retCode=EXIT_SUCCESS;
+    cv::Mat input;
+    cv::Mat output;
+    cv::Mat mask;
+    int r;
+    double g;
+    int filter_type;
+    bool circular;
+};
 
-    try {
+/** @brief Standard trackbar callback
+ * Use this function an argument for cv::createTrackbar to control
+ * the trackbar changes.
+ *
+ * @arg v give the trackbar position.
+ * @arg user_data allow to pass user data to the callback.
+ */
+void on_change(int v, void *user_data_)
+{
+    UserData *user_data = static_cast<UserData *>(user_data_);
+
+    cv::Mat in, out;
+
+    user_data->r = v;
+
+    in = user_data->input;
+    out = user_data->output;
+
+    in.convertTo(in, CV_32F, 1.0 / 255.0);
+    std::vector<cv::Mat> chs;
+    cv::Mat in_;
+
+    if (in.channels() == 3)
+    {
+        cv::Mat hsv;
+        cv::cvtColor(in, hsv, cv::COLOR_BGR2HSV);
+        cv::split(hsv, chs);
+        in_ = chs[2];
+    }
+    else
+    {
+        in_ = in;
+    }
+
+    out = fsiv_usm_enhance(in_, user_data->g, user_data->r, user_data->filter_type, user_data->circular, &user_data->mask);
+
+    if (in.channels() == 3)
+    {
+        chs[2] = out;
+        cv::Mat hsv;
+        cv::merge(chs, hsv);
+        cv::cvtColor(hsv, out, cv::COLOR_HSV2BGR);
+    }
+
+    cv::imshow("INPUT", in);
+    cv::imshow("OUTPUT", out);
+    cv::imshow("UNSHARP MASK", user_data->mask);
+    // cv::waitKey(0);
+
+    //
+}
+
+int main(int argc, char *const *argv)
+{
+    int retCode = EXIT_SUCCESS;
+
+    try
+    {
         cv::CommandLineParser parser(argc, argv, keys);
         parser.about("Apply an unsharp mask enhance to an image.");
         if (parser.has("help"))
@@ -58,16 +124,17 @@ main (int argc, char* const* argv)
         //TODO
         double g = 1.0;
         int r = 1;
-        int filter_type=1;
-        
-        if(parser.has("gain"))
+        int filter_type = 1;
+
+        if (parser.has("gain"))
             g = parser.get<double>("gain");
-        if(parser.has("radius"))
+        if (parser.has("radius"))
             r = parser.get<int>("radius");
-        if(parser.has("filter") && (parser.get<int>("filter") == 0 || parser.get<int>("filter") == 1) )
-            filter_type= parser.get<int>("filter");
-        
-        bool circular =parser.has("circular");
+        if (parser.has("filter") && (parser.get<int>("filter") == 0 || parser.get<int>("filter") == 1))
+            filter_type = parser.get<int>("filter");
+
+        bool circular = parser.has("circular");
+        bool interactive = parser.has("interactive");
 
         //
 
@@ -86,40 +153,57 @@ main (int argc, char* const* argv)
         cv::namedWindow("OUTPUT");
         cv::namedWindow("UNSHARP MASK");
 
+        UserData user_data;
+
         /**/
-        in.convertTo(in, CV_32F, 1.0/255.0);
-        std::vector<cv::Mat> chs;
-        cv::Mat in_;
-
-        if(in.channels()==3){
-            cv::Mat hsv;
-            cv::cvtColor(in,hsv,cv::COLOR_BGR2HSV);
-            cv::split(hsv,chs);
-            in_ = chs[2];
+        if (interactive)
+        {
+            user_data.input = in;
+            user_data.output = out;
+            user_data.r = r;
+            user_data.g = g;
+            user_data.filter_type = filter_type;
+            user_data.circular = circular;
+            cv::createTrackbar("Radius", "INPUT", &r, 100, on_change, &user_data);
         }
-        else{
-            in_ = in;
+        else
+        {
+            in.convertTo(in, CV_32F, 1.0 / 255.0);
+            std::vector<cv::Mat> chs;
+            cv::Mat in_;
+
+            if (in.channels() == 3)
+            {
+                cv::Mat hsv;
+                cv::cvtColor(in, hsv, cv::COLOR_BGR2HSV);
+                cv::split(hsv, chs);
+                in_ = chs[2];
+            }
+            else
+            {
+                in_ = in;
+            }
+
+            out = fsiv_usm_enhance(in_, g, r, filter_type, circular, &mask);
+
+            if (in.channels() == 3)
+            {
+                chs[2] = out;
+                cv::Mat hsv;
+                cv::merge(chs, hsv);
+                cv::cvtColor(hsv, out, cv::COLOR_HSV2BGR);
+            }
         }
 
+        cv::imshow("INPUT", in);
+        cv::imshow("OUTPUT", out);
+        cv::imshow("UNSHARP MASK", mask);
 
-        out = fsiv_usm_enhance(in_, g, r, filter_type, circular, &mask);
-
-        if(in.channels() == 3){
-            chs[2] = out;
-            cv::Mat hsv;
-            cv::merge(chs,hsv);
-            cv::cvtColor(hsv,out,cv::COLOR_HSV2BGR);
-        }
-
-        cv::imshow ("INPUT", in);
-        cv::imshow ("OUTPUT", out);
-        cv::imshow ("UNSHARP MASK", mask);
-
-        int k = cv::waitKey(0)&0xff;
-        if (k!=27)
+        int k = cv::waitKey(0) & 0xff;
+        if (k != 27)
             cv::imwrite(output_n, out);
     }
-    catch (std::exception& e)
+    catch (std::exception &e)
     {
         std::cerr << "Capturada excepcion: " << e.what() << std::endl;
         retCode = EXIT_FAILURE;
